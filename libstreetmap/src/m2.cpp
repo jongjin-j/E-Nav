2e7cb9ba05f04aff215bc44cfee04ea4fdb5ef48
Seong Joo Yoon <seongjoo.yoon@mail.utoronto.ca>
2021-03-06 03:55:53 -0500

    Added helper function to draw open features

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -13,21 +13,23 @@
 #include "rectangle.hpp"
 #include <math.h>
 #include "globals.h"
+#include <typeinfo>
+#include <string>
 
-struct intersection_data{
+struct intersection_data {
     std::string name;
     double x = 0;
     double y = 0;
     bool highlight = false;
 };
 
-struct POI_data{
+struct POI_data {
     std::string name;
     double x = 0;
     double y = 0;
 };
 
-struct street_data{
+struct street_data {
     std::string name;
     double start_x = 0;
     double start_y = 0;
@@ -42,24 +44,50 @@ struct street_data{
 
 float avg_lat;
 
-double x_from_lon(double lon){
+double x_from_lon(double lon) {
     double x = kEarthRadiusInMeters * kDegreeToRadian * std::cos(kDegreeToRadian * avg_lat) * (lon);
     return x;
 }
 
-double y_from_lat(double lat){
+double y_from_lat(double lat) {
     double y = kEarthRadiusInMeters * kDegreeToRadian * lat;
     return y;
 }
 
-double lon_from_x(double x){
-    double lon = x/(kEarthRadiusInMeters * kDegreeToRadian * std::cos(kDegreeToRadian * avg_lat));
+double lon_from_x(double x) {
+    double lon = x / (kEarthRadiusInMeters * kDegreeToRadian * std::cos(kDegreeToRadian * avg_lat));
     return lon;
 }
 
-double lat_from_y(double y){
-    double lat = y/(kEarthRadiusInMeters * kDegreeToRadian);
+double lat_from_y(double y) {
+    double lat = y / (kEarthRadiusInMeters * kDegreeToRadian);
     return lat;
+}
+//helper function to choose colour from feature type
+std::string setColour(FeatureType x) {
+    
+    if (x == UNKNOWN) {
+        return "grey";
+    } else if (x == PARK) {
+        return "green";
+    } else if (x == BEACH) {
+        return "bisque";
+    }  else if (x == LAKE) {
+        return "blue";
+    }  else if (x == RIVER) {
+        return "blue";
+    }  else if (x == ISLAND) {
+        return "bisque";
+    }  else if (x == BUILDING) {
+        return "grey";
+    }  else if (x == GREENSPACE) {
+        return "green";
+    }  else if (x == GOLFCOURSE) {
+        return "green";
+    }  else if (x == STREAM) {
+        return "blue";
+    } 
+
 }
 
 std::vector<intersection_data> intersections;
@@ -67,46 +95,54 @@ std::vector<POI_data> POIs;
 std::vector<std::vector<StreetSegmentIdx>> streetSegments;
 std::vector<street_data> streets;
 
-void draw_main_canvas(ezgl::renderer *g){
-    g->draw_rectangle({0, 0}, {1000, 1000});
-    
+void draw_main_canvas(ezgl::renderer *g) {
+    g->draw_rectangle({0, 0},
+    {
+        1000, 1000
+    });
+
     ezgl::rectangle scope = g->get_visible_world();
     double scope_length = scope.m_second.x - scope.m_first.x;
     double scope_height = scope.m_second.y - scope.m_first.y;
 
-    for(int id = 0; id < intersections.size(); id++){
+    for (int id = 0; id < intersections.size(); id++) {
         float x = intersections[id].x;
         float y = intersections[id].y;
         float width = 10;
         float height = width;
-        
-        if (intersections[id].highlight && scope_length < 800){
+
+        if (intersections[id].highlight && scope_length < 800) {
             //print name of intersection
-            ezgl::point2d center_point(x,y+7.5);
+            ezgl::point2d center_point(x, y + 7.5);
             g->set_color(ezgl::BLACK);
             g->set_font_size(13);
             g->draw_text(center_point, intersections[id].name);
             std::cout << "Closest Intersection: " << intersections[id].name << "\n";
-            
+
             //set color for intersection icon 
             g->set_color(ezgl::RED);
-        }
-        else{ 
+        } else {
             g->set_color(ezgl::GREY_55);
         }
-        
-        g->fill_rectangle({x - width/2, y - height/2}, {x + width/2, y + height/2});
+
+        g->fill_rectangle({x - width / 2, y - height / 2},
+        {
+            x + width / 2, y + height / 2
+        });
     }
 
     //drawing streets
-    for(int i = 0; i < getNumStreetSegments(); i++){
+    for (int i = 0; i < getNumStreetSegments(); i++) {
         g->set_line_width(2);
         g->set_color(ezgl::GREY_55);
-        g->draw_line({streets[i].start_x, streets[i].start_y}, {streets[i].end_x, streets[i].end_y});
-        
-        if (scope_length < 500 && scope_height < 400 && findStreetSegmentLength(i) > 70){
-            if(scope.m_first.x < streets[i].mid_x && scope.m_second.x > streets[i].mid_x && scope.m_first.y < streets[i].mid_y && scope.m_second.y > streets[i].mid_y){
-                ezgl::point2d centerPoint (streets[i].mid_x, streets[i].mid_y);
+        g->draw_line({streets[i].start_x, streets[i].start_y},
+        {
+            streets[i].end_x, streets[i].end_y
+        });
+
+        if (scope_length < 500 && scope_height < 400 && findStreetSegmentLength(i) > 70) {
+            if (scope.m_first.x < streets[i].mid_x && scope.m_second.x > streets[i].mid_x && scope.m_first.y < streets[i].mid_y && scope.m_second.y > streets[i].mid_y) {
+                ezgl::point2d centerPoint(streets[i].mid_x, streets[i].mid_y);
                 g->set_text_rotation(streets[i].angle);
                 g->set_font_size(15);
                 g->set_color(ezgl::BLACK);
@@ -114,172 +150,164 @@ void draw_main_canvas(ezgl::renderer *g){
             }
         }
     }
-    
+
     //drawing features
-    for(int i = 0; i < getNumFeatures(); i++){
+    for (int i = 0; i < getNumFeatures(); i++) {
 
         //if it's a closed feature
-        if(getFeaturePoint(i,0) == getFeaturePoint(i, getNumFeaturePoints(i)-1)){
-           
+        if (getFeaturePoint(i, 0) == getFeaturePoint(i, getNumFeaturePoints(i) - 1)) {
+
             //declare vector of 2d points
-            std::vector<ezgl::point2d> featurePoints;
+            /*std::vector<ezgl::point2d> featurePoints;
+            featurePoints.resize(getNumFeaturePoints(i), {0,0});
             
             
             //loop through # feature points and add to vector of 2d points
-            for(int j = 0; j < getNumFeaturePoints(i)-1; j++){
+            for(int j = 0; j < getNumFeaturePoints(i); j++){
                 double xCoord = x_from_lon(getFeaturePoint(i,j).longitude());
                 double yCoord = y_from_lat(getFeaturePoint(i,j).latitude());
                 
-                ezgl::point2d myPoint = {xCoord,yCoord};
-
-                featurePoints.push_back(myPoint);
-                g->draw_line(myPoint,{x_from_lon(getFeaturePoint(i,j+1).longitude()),y_from_lat(getFeaturePoint(i,j+1).latitude())});
+                featurePoints[j] = {xCoord,yCoord};
             }
-            
+
             //fill poly used to colour the set of points
-            //g->fill_poly(featurePoints);
-     
-        }
-        else{
+            g->fill_poly(featurePoints);*/
+
+        } else {
             //open feature
             //draw with open lines
-            for(int j = 0; j < getNumFeaturePoints(i)-1; j++){
-                      
-                double xCoord = x_from_lon(getFeaturePoint(i,j).longitude());
-                double yCoord = y_from_lat(getFeaturePoint(i,j).latitude());
-                                   
+            for (int j = 0; j < getNumFeaturePoints(i) - 1; j++) {
+
+                double xCoord = x_from_lon(getFeaturePoint(i, j).longitude());
+                double yCoord = y_from_lat(getFeaturePoint(i, j).latitude());
+
                 //choose colour depending on feature type
-                /*if(getFeatureType(i) == UNKNOWN){
-                    g->set_color(ezgl::GREY_55);
-                }else if(getFeatureType(i) == (PARK || GREENSPACE || GOLFCOURSE)){
-                    g->set_color(ezgl::GREEN);
-                }else if(getFeatureType(i) == BEACH){
-                    g->set_color(ezgl::YELLOW);
-                }else if(getFeatureType(i) == (LAKE || RIVER || RIVER || STREAM)){
+                if(setColour(getFeatureType(i)) == "blue"){
                     g->set_color(ezgl::BLUE);
-                }else if(getFeatureType(i) == ISLAND){
+                }else if(setColour(getFeatureType(i)) == "green"){
+                    g->set_color(ezgl::GREEN);
+                }else if(setColour(getFeatureType(i)) == "bisque"){
+                    g->set_color(ezgl::BISQUE);
+                }else if(setColour(getFeatureType(i)) == "grey"){
                     g->set_color(ezgl::GREY_55);
-                }else if(getFeatureType(i) == BUILDING){
-                    g->set_color(ezgl::GREY_55);
-                }*/
-                //g->set_color(ezgl::BLUE);
-                //g->draw_line({xCoord,yCoord},{x_from_lon(getFeaturePoint(i,j+1).longitude()),y_from_lat(getFeaturePoint(i,j+1).latitude())});
+                }
+                
+                g->draw_line({xCoord, yCoord},
+                {
+                    x_from_lon(getFeaturePoint(i, j + 1).longitude()), y_from_lat(getFeaturePoint(i, j + 1).latitude())
+                });
             }
 
         }
     }
-    
-    
+
     //drawing POIs
-    
-    for(int i = 0; i < POIs.size(); i++){
+
+    for (int i = 0; i < POIs.size(); i++) {
         float radius = 5;
-        
+
         g->set_color(ezgl::BLUE);
         g->set_text_rotation(0);
-        
+
         ezgl::point2d center(POIs[i].x, POIs[i].y);
-        
-        g->fill_elliptic_arc (center, radius, radius, 0, 360);
-        
+
+        g->fill_elliptic_arc(center, radius, radius, 0, 360);
+
         ezgl::point2d center_point(POIs[i].x, POIs[i].y + 7.5);
 
-        if(scope_length < 85 && scope_height < 70){
+        if (scope_length < 85 && scope_height < 70) {
             g->set_color(ezgl::BLACK);
             g->set_font_size(16);
             g->draw_text(center_point, POIs[i].name);
         }
-        
-        else if(scope_length < 240 && scope_height < 185){
+        else if (scope_length < 240 && scope_height < 185) {
             g->set_color(ezgl::BLACK);
             g->set_font_size(13);
             g->draw_text(center_point, POIs[i].name);
         }
-        
-        else if(scope_length < 385 && scope_height < 305){
+        else if (scope_length < 385 && scope_height < 305) {
             g->set_color(ezgl::BLACK);
             g->set_font_size(10);
             g->draw_text(center_point, POIs[i].name);
         }
-        
-        else if(scope_length < 650 && scope_height < 505){
+        else if (scope_length < 650 && scope_height < 505) {
             g->set_color(ezgl::BLACK);
             g->set_font_size(8);
             g->draw_text(center_point, POIs[i].name);
         }
     }
-    
+
     //make the search box for street intersections
-    
+
 }
 
-void act_on_mouse_click(ezgl::application* app, GdkEventButton* event, double x, double y){
-    
+void act_on_mouse_click(ezgl::application* app, GdkEventButton* event, double x, double y) {
+
     LatLon pos = LatLon(lat_from_y(y), lon_from_x(x));
     int id = findClosestIntersection(pos);
-    
+
     if (intersections[id].highlight == true)
         intersections[id].highlight = false;
     else
         intersections[id].highlight = true;
-    
+
     app -> refresh_drawing();
 }
 
-void drawMap(){
+void drawMap() {
     ezgl::application::settings settings;
     settings.main_ui_resource = "libstreetmap/resources/main.ui";
     settings.window_identifier = "MainWindow";
     settings.canvas_identifier = "MainCanvas";
-    
+
     ezgl::application application(settings);
-    
+
     //set intersections database
     double max_lat = getIntersectionPosition(0).latitude();
     double min_lat = max_lat;
     double max_lon = getIntersectionPosition(0).longitude();
     double min_lon = max_lon;
-    
+
     intersections.resize(getNumIntersections());
-    
+
     //set name in database and find the max and min lat, lon
-    for(int i = 0; i < getNumIntersections(); i++){
+    for (int i = 0; i < getNumIntersections(); i++) {
         intersections[i].name = getIntersectionName(i);
-        
+
         max_lat = std::max(max_lat, getIntersectionPosition(i).latitude());
         min_lat = std::min(min_lat, getIntersectionPosition(i).latitude());
         max_lon = std::max(max_lon, getIntersectionPosition(i).longitude());
         min_lon = std::min(min_lon, getIntersectionPosition(i).longitude());
-    } 
-    
+    }
+
     //average lat for Cartesian transformation
-    avg_lat = (min_lat + max_lat)/2;
-    
+    avg_lat = (min_lat + max_lat) / 2;
+
     //change intersection points to Cartesian coordinates
-    for(int i = 0; i < getNumIntersections(); i++){
+    for (int i = 0; i < getNumIntersections(); i++) {
         intersections[i].x = x_from_lon(getIntersectionPosition(i).longitude());
         intersections[i].y = y_from_lat(getIntersectionPosition(i).latitude());
     }
-    
+
     //set POI database
     POIs.resize(getNumPointsOfInterest());
-    
-    for (int i = 0; i < getNumPointsOfInterest(); i++){
+
+    for (int i = 0; i < getNumPointsOfInterest(); i++) {
         POIs[i].name = getPOIName(i);
         POIs[i].x = x_from_lon(getPOIPosition(i).longitude());
         POIs[i].y = y_from_lat(getPOIPosition(i).latitude());
     }
-    
+
     //
     streets.resize(getNumStreetSegments());
-    
-    for(int i = 0; i < getNumStreetSegments(); i++){
-        
+
+    for (int i = 0; i < getNumStreetSegments(); i++) {
+
         //for each street segment, obtain its intersection IDs "from" and "to"
         //obtain each intersection ID's position via calling getIntersectionPosition (type LatLon)
-        LatLon startingSeg = LatLon(getIntersectionPosition(getStreetSegmentInfo(i).from).latitude(),getIntersectionPosition(getStreetSegmentInfo(i).from).longitude());
-        LatLon endingSeg = LatLon(getIntersectionPosition(getStreetSegmentInfo(i).to).latitude(),getIntersectionPosition(getStreetSegmentInfo(i).to).longitude());
-        
+        LatLon startingSeg = LatLon(getIntersectionPosition(getStreetSegmentInfo(i).from).latitude(), getIntersectionPosition(getStreetSegmentInfo(i).from).longitude());
+        LatLon endingSeg = LatLon(getIntersectionPosition(getStreetSegmentInfo(i).to).latitude(), getIntersectionPosition(getStreetSegmentInfo(i).to).longitude());
+
         //convert LatLon into Cartesian coord and draw line for each segment
         streets[i].start_x = x_from_lon(startingSeg.longitude());
         streets[i].start_y = y_from_lat(startingSeg.latitude());
@@ -289,22 +317,20 @@ void drawMap(){
         streets[i].mid_y = 0.5 * (streets[i].start_y + streets[i].end_y);
 
         double rotation = 0;
-            
-        if(streets[i].end_x == streets[i].start_x){
+
+        if (streets[i].end_x == streets[i].start_x) {
             rotation = 90;
         }
-            
-        else{
+        else {
             rotation = std::atan(abs((streets[i].end_y - streets[i].start_y) / (streets[i].end_x - streets[i].start_x))) / kDegreeToRadian;
         }
-        
-        if ((streets[i].end_x > streets[i].start_x && streets[i].end_y > streets[i].start_y) || (streets[i].end_x < streets[i].start_x && streets[i].end_y < streets[i].start_y)){
+
+        if ((streets[i].end_x > streets[i].start_x && streets[i].end_y > streets[i].start_y) || (streets[i].end_x < streets[i].start_x && streets[i].end_y < streets[i].start_y)) {
             streets[i].angle = rotation;
-        }
-        else{
+        } else {
             streets[i].angle = -1 * rotation;
         }
-        
+
         streets[i].name = getStreetName(getStreetSegmentInfo(i).streetID);
         
         //setting the types of street segments
@@ -329,10 +355,13 @@ void drawMap(){
             }
         } 
     }
-    
-    ezgl::rectangle initial_world({x_from_lon(min_lon), y_from_lat(min_lat)}, {x_from_lon(max_lon), y_from_lat(max_lat)});
+
+    ezgl::rectangle initial_world({x_from_lon(min_lon), y_from_lat(min_lat)},
+    {
+        x_from_lon(max_lon), y_from_lat(max_lat)
+    });
     application.add_canvas("MainCanvas", draw_main_canvas, initial_world);
-    
+
     application.run(nullptr, act_on_mouse_click, nullptr, nullptr);
 }
 

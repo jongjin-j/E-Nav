3914e38f5297d4ca2e6434c8004b4c478fbda2cf
Jongjin <jongjin.jung@mail.utoronto.ca>
2021-03-28 20:17:32 -0400

    Created m3.cpp

---
+++ libstreetmap/src/m3.cpp
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <iostream>
+#include "m1.h"
+#include "globals.h"
+#include "StreetsDatabaseAPI.h"
+#include <string>
+#include <string.h>
+#include "m3.h"
+
+double computePathTravelTime(const std::vector<StreetSegmentIdx>& path, const double turn_penalty){
+    
+}
+
+std::vector<StreetSegmentIdx> findPathBetweenIntersections(const IntersectionIdx intersect_id_start, 
+const IntersectionIdx intersect_id_destination, const double turn_penalty){
+    
+}
+
